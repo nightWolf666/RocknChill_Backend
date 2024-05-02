7b80c88b71b82cbebcabc6932c559080177d40ff
nightWolf666 <chsturm75@gmail.com>
2024-05-02 17:28:40 +0200

    reworked backend without auth

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -19,14 +19,17 @@ export async function insertNewEvent(req, res) {
 export async function updateEvent(req, res) {
     const {id} = req.params;
     console.log(id);
-    const { event_name,event_ort,event_start,event_ende,event_beschreibung_kurz,event_beschreibung_lang,event_genre,event_link,event_bild } = req.body;
+    const { event_name,event_ort,event_start,event_ende,event_beschreibung_kurz,event_beschreibung_lang,event_genre,event_link,event_bild,user_id } = req.body;
     console.log(req.body)
     try {
-        const { rows: event } = await pool.query(
-            'UPDATE  event SET event_name = $1, event_ort = $2, event_start = $3, event_ende = $4, event_beschreibung_kurz = $5, event_beschreibung_lang = $6,event_genre = $7, event_link = $8, event_bild = $9  WHERE user_id = $10 RETURNING *',
-            [event_name,event_ort,event_start,event_ende,event_beschreibung_kurz,event_beschreibung_lang,event_genre,event_link,event_bild,id]
+        
+        const { rows: event } = await pool.query(    
+            'UPDATE  event SET event_name = $1, event_ort = $2, event_start = $3, event_ende = $4, event_beschreibung_kurz = $5, event_beschreibung_lang = $6,event_genre = $7, event_link = $8, event_bild = $9, user_id = $10  WHERE event_id = $11 RETURNING *',
+            [event_name,event_ort,event_start,event_ende,event_beschreibung_kurz,event_beschreibung_lang,event_genre,event_link,event_bild,user_id,id]
         );
         res.json(event);
+        console.log(event);
+
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
